bd4d9e7d090d50a5243489da630ab1962282c86b
David Azevedo <65208891+Zevid-Lucas@users.noreply.github.com>
2021-08-06 19:54:52 -0300

    [Main group 16] [requisito 15] (#467)
    * Cria botao para jogar novamente, enviando a pessoa para a login
    
    * Cria botao para jogar novamente, enviando a pessoa para a login

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FeedBackHeader from '../components/FeedBackHeader';
 
 class Feedback extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <FeedBackHeader />
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ () => history.push('/') }
+        >
+          Jogar novamente
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape(Object).isRequired,
+};
 
 export default Feedback;
