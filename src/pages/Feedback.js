df997e37a11926257190eba2f09573d7e0aeb2a3
Matheus Tkaczyk Ribeiro <82241753+matheustkaczyk@users.noreply.github.com>
2021-08-06 21:17:02 -0300

    Requisito 14 concluido, tela de feedback mostrando todas as informaçoes (#476)

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import FeedBackHeader from '../components/FeedBackHeader';
 
 class Feedback extends React.Component {
   render() {
-    const { history } = this.props;
+    const { history, assertions, score } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <FeedBackHeader />
+        <h3 data-testid="feedback-total-score">{score}</h3>
+        <h3 data-testid="feedback-total-question">{assertions}</h3>
         <button
           data-testid="btn-play-again"
           type="button"
@@ -23,6 +26,13 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   history: PropTypes.shape(Object).isRequired,
+  assertions: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
 };
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.assertionsReducer.assertions,
+  score: state.scoreReducer.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
