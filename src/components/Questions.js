6773af7935492814d79e5682807e1fdb9d8b1707
David Azevedo <65208891+Zevid-Lucas@users.noreply.github.com>
2021-08-06 21:18:04 -0300

    [Group 16][Fix] (#475)
    * Cria botao para jogar novamente, enviando a pessoa para a login
    
    * Correcao: Exibe botao de proximo e questoes corretas e erradas quadno o tempo zera

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -171,7 +171,7 @@ class Questions extends Component {
                 key={ `${question.id}` }
                 disabled={ isDisabled }
                 onClick={ this.verifyAns }
-                className={ btnClicked
+                className={ btnClicked || timer === 0
                   ? question.ifCorrect
                   : '' }
               >
@@ -180,8 +180,8 @@ class Questions extends Component {
             ))
           }
         </div>
-        { nextQuestion
-        && <ButtonNext testId="btn-next" changeQuestion={ this.changeQuestion } /> }
+        { nextQuestion || timer === 0
+          ? <ButtonNext testId="btn-next" changeQuestion={ this.changeQuestion } /> : ''}
 
       </>
     );
